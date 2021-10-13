5d4e27766631efb02b9359fdcc92e85c281ec9e9
ntlinh0402 <84694214+ntlinh0402@users.noreply.github.com>
2021-10-13 14:40:50 +0700

    Add files via upload

---
+++ Day4/index.js
@@ -0,0 +1,51 @@
+// alert("Hello");
+// let name=prompt("What's your name");
+// let address=prompt("Where do you live?")
+// alert("Your name is" + name + "live in" + address);
+// console.log(name);
+// console.log(address);
+// let year=prompt("Insert year");
+// let age=2021-Number(year);
+// if(age>30){
+//     alert('middle age and old')
+// }else if(age>=18){
+//     alert('adult')
+// }else if (age>=15){
+//     alert('teenager')
+// }else if(age>=10){
+//     alert('child')
+// }else if(age>=2){
+//     alert("baby")
+// }
+// let year = prompt("Insert");
+// let  = Number(femaleStudent) + Number(maleStudent);
+// alert(total);
+// console.log(year);
+// let age("How old are u?");
+// if(age>30){
+//     alert('middle age and old')
+// }else if(age>=18){
+//     alert('adult')
+// }else if (age>15){
+//     alert('teenager')
+// }
+
+//HOMEWORK
+//Ex1
+var firstName = prompt("Enter your first name");
+var surName = prompt("Enter your surname");
+var lastName = prompt("Enter your last name");
+var name = lastName + " " + surName + " " + firstName;
+console.log(name);
+alert(name);
+//Ex1
+var a = prompt("Enter dividend")
+var b = prompt("Enter divisor")
+var c = a%b;
+alert("remainder" + " "+ c)
+console.log(c)
+//Ex3
+var myString=prompt("Enter your string:");
+var length=myString.length;
+alert(length)
+
